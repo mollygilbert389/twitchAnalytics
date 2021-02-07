08948bf0bf075ddbc2ffb5d01c4dccef59915198
Molly Gilbert <msgilbert389@gmail.com>
2021-02-07 14:03:54 -0600

    adding API info

---
+++ app.js
@@ -0,0 +1,23 @@
+const key = "ruzxqvfrbqh59br4qkpsm46eporuj5"
+const secret = "i2k9i1l7wuzy8kcg79cnhhgyd60kal"
+
+function getTwitchData() {
+    fetch('https://id.twitch.tv/oauth2/validate', {
+	headers: {
+        "Authorization": "Bearer " + secret,
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.twitchtv.v5+json'
+	}
+	}).then(function (response) {
+        console.log(response)
+		return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+};
+
+getTwitchData();
